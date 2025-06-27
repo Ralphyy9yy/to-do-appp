84bccd0aba158dbf655237cb850607351766fd5a
Jiroo <ralph_giann.suquib@bisu.edu.ph>
2025-06-27 21:04:46 +0800

    initial commit

---
+++ server/routes/task.js
@@ -0,0 +1,34 @@
+const express = require ("express");
+const router = express.Router();
+const Task = require("../models/Task");
+
+//get all tasks
+router.get("/", async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+//add new task
+router.post("/", async (req, res) => {
+    const {title} = req.body;
+    const newTask = new Task ({title});
+    await newTask.save();
+    res.json(newTask);
+
+});
+
+//toggle task completion
+router.put("/:id", async (res,req) => {
+    const task =await Task.findById(req.params.id);
+    task.completed = !taskk.completed;
+    await task.save();
+    res.json(task);
+});
+
+//delete task
+router.delete("/:id", async (req,res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({message: "Deleted"});
+});
+
+module.exports = router;
